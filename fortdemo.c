8f33d10b0b39ef3cd4777f5ab03bdc7cf60d5fcc
harani1 <79127679+harani1@users.noreply.github.com>
2021-02-23 15:10:58 +0530

    Create fortdemo.c
    fhgj

---
+++ fortdemo.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<sys/types.h>
+#include<unistd.h>
+int main()
+{
+int pid=0;
+pid=fork();
+if(pid==0)
+{
+printf("in child");
+}
+else
+{
+printf("In parent process id of child is %d");
+return 0;
+}
+}
